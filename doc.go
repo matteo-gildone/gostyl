ff89ef4b2e3de5f42d60d138471f848a923051dd
matteo-gildone <matteo.gildone@springernature.com>
2026-02-23 14:28:45 +0000

    chore: more tests

--- doc.go
+++ doc.go
@@ -1,10 +1,10 @@
-// Package gostyl provide a tiny, zero dependency ANSI styling helper for Go CLI applications
+// Package gostyl provides a tiny, zero dependency ANSI styling helper for Go CLI applications
 //
-// Color output is automatically disabled when NO_COLOR environment variable is set or when TERM is "dump" or empty.
+// Color output is automatically disabled when NO_COLOR environment variable is set or when TERM is "dumb" or empty.
 //
 // Basic usage:
 //
 //	s := gostyl.NewStyle()
-//	fmt.Prinln(s.Bold().Red().Sprint("error"))
-//	fmt.Prinln(gostyl.Danger().Sprint("something went wrong"))
+//	fmt.Println(s.Bold().Red().Sprint("error"))
+//	fmt.Println(gostyl.Danger().Sprint("something went wrong"))
 package gostyl
--- presets.go
+++ presets.go
@@ -1,17 +1,61 @@
 package gostyl
 
 func Success(s string) string {
-	return NewStyle().Green().Sprint(s)
+	return NewStyle().BrightGreen().Sprint("✓ " + s)
 }
 
 func Successf(format string, a ...any) string {
-	return NewStyle().Green().Sprintf(format, a)
+	return NewStyle().BrightGreen().Sprintf("✓ "+format, a...)
 }
 
-func SuccessWithIcon(s string) string {
-	return NewStyle().Green().Sprint("✓ " + s)
+func Successln(s string) string {
+	return NewStyle().BrightGreen().Sprintln("✓ " + s)
 }
 
-func SuccessWithIconf(format string, a ...any) string {
-	return NewStyle().Green().Sprintf("✓ "+format, a)
+func Warning(s string) string {
+	return NewStyle().BrightYellow().Sprint("! " + s)
+}
+
+func Warningf(format string, a ...any) string {
+	return NewStyle().BrightYellow().Sprintf("! "+format, a...)
+}
+
+func Warningln(s string) string {
+	return NewStyle().BrightYellow().Sprintln("! " + s)
+}
+
+func Danger(s string) string {
+	return NewStyle().BrightRed().Sprint("x " + s)
+}
+
+func Dangerf(format string, a ...any) string {
+	return NewStyle().BrightRed().Sprintf("x "+format, a...)
+}
+
+func Dangerln(s string) string {
+	return NewStyle().BrightRed().Sprintln("x " + s)
+}
+
+func Info(s string) string {
+	return NewStyle().BrightCyan().Sprint("i " + s)
+}
+
+func Infof(format string, a ...any) string {
+	return NewStyle().BrightCyan().Sprintf("i "+format, a...)
+}
+
+func Infoln(s string) string {
+	return NewStyle().BrightCyan().Sprintln("i " + s)
+}
+
+func Muted(s string) string {
+	return NewStyle().BrightBlack().Sprint(s)
+}
+
+func Mutedf(format string, a ...any) string {
+	return NewStyle().BrightBlack().Sprintf(format, a...)
+}
+
+func Mutedln(s string) string {
+	return NewStyle().BrightBlack().Sprintln(s)
 }
